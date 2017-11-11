70008736ec222705821bffde86ffa82d27743c98
UNIT-2\Dvors <muraviev.sergey@icloud.com>
2017-11-11 13:59:23 +0300

    абв

---
+++ Wizzard/Assets/Scripts/Story.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Story : MonoBehaviour {
+
+	enum States 
+	{
+		kidnap,
+		death,
+		totrture,
+		bedEnd,
+		goodEnd
+	}
+	States stete = States.kidnap;
+	public Text maniText;
+
+	// Update is called once per frame
+	void Kidnap () {
+		maniText.text = "тебя схватили и закинули в багажник\n" +
+		"(П)Попытаться сбежать\n (Н)Ничего не делать";
+		if (Input.GetKeyDown(KeyCode.Y)) {
+			stete = States.death;
+		}
+		if (Input.GetKeyDown(KeyCode.G)) {
+			stete = States.totrture;
+		}
+
+	}
+	void Death () {
+		maniText.text = "Вы попытались сбежать вас убили.Конец\n"+"(З)Заново\n.";
+		if (Input.GetKeyDown(KeyCode.P)) 
+			{
+			stete = States.kidnap;
+		}
+
+	}
+	void Update() {
+		if (stete == States.death) {
+			Death ();
+		}
+		if(stete == States.kidnap) {
+			Kidnap ();
+		}
+}
+}
